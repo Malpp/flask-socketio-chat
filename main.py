401b27096ea3d59a97c4514feab0059a897da32b
Malpp <cpt.ortan@gmail.com>
2017-11-23 14:47:57 -0500

    Started image threads

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask 
 from flask_socketio import SocketIO, send
+from threading import Thread
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
+
+threads = []
+
+def downloadImage(url):
+    print("Hello")
+    print(len(threads))
+    pass
 
 @app.route('/')
 def hello_world():
@@ -14,6 +22,12 @@ def hello_world():
 def handleMessage(msg):
 	print('Message: ' + msg)
 	send(msg, broadcast=True)
+
+@socketio.on('image')
+def handleMessage(msg):
+    print('URL: ' + msg)
+    threads.append(Thread(target=downloadImage, args=(msg,)).start())
+	#send(msg, broadcast=True)
 
 if __name__ == '__main__':
 	socketio.run(app)
